2aaa6d60ae36a439e52b794b30263c12444c8858
NOT-PRO-AMAN <amansingh484748@gmail.com>
2024-08-10 18:09:13 +0530

    created the portal for user input

--- LMS.py
+++ LMS.py
@@ -1,3 +1,5 @@
+
+
 class Book:
     def __init__(self, title, author):
         self.title = title
@@ -35,23 +37,32 @@ class library:
         self.books.append(book)
         print(f"\n++++++++++\nbook returned and added {book}\n++++++++++")
 
-       
+l = library()      
+
+def user_input():
+    while True:
+        a = int(input("--------------------\nWelcome to the Library-:\nChoose the adjecent number of the task you want to preform.\n(1)-Display books\n(2)-borrow books\n(3)-Return book\n--->>>"))
+
+
+        if a == 1 :
+            l.display_books()
+        elif a == 2:
+            b = input("Enter the name of book you want -->").lower()
+            l.borrow(b)
+
+        elif a == 3:
+            c = input("Name of the Book-->").lower()
+            d = input("Name of the Author-->").lower()
+            l.return_book(Book(c,d))
+
+        else:
+            print("write a valid integer")
 
 
 
-
-l = library()
 l.add_book(Book("the adventure","aman singh"))
 l.add_book(Book("war and peace","leo tolstoy"))
 l.add_book(Book("utopia","sir thomas moor"))
-l.add_book(Book("romio and juliet","William Shakespeare"))
-
-l.display_books()
-
-l.borrow("the adventure")
-
-l.display_books()
-
-l.return_book(Book("the adventure","aman singh"))
-
-l.display_books()
+l.add_book(Book("romio and juliet","William Shakespear"))
+           
+user_input()
